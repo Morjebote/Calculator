202fb77ea980dcbe328f12b53ff1d903cd9fc593
Mor <104593221+Morjebote@users.noreply.github.com>
2023-01-08 07:12:11 +0100

    Update index.js
    Calculator 2.0, completely new js code

--- Files/index.js
+++ Files/index.js
@@ -1,197 +1,188 @@
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const operators = ["+", "-", "*", "/"];
-let seq = {first:[], second:[]};
-let num = {first:0, second:0};
-let res = [];
 let lastClicked = 0;
-let error = false;
 let activeArray = "first";
-
-activateAll();
-
-$(".reset").click(function(event){
-  if(lastClicked != "c"){
-    seq = {first:[], second:[]};
-    num = {first:0, second:0};
-    res = [];
-    activeArray = "first";
-    $(".field").text(0);
-    markLast();
-    if(error == true){
-      activateAll();
-      error = false;
+let error = false;
+let seq = { first: [], second: [] };
+let num = {
+  first: function () { return Number(seq.first.join('')) },
+  second: function () { return Number(seq.second.join('')) },
+  operator: 0,
+  result: function () {
+    if (num.operator == "+") {
+      return num.first() + num.second();
+    } else if (num.operator == "-") {
+      return num.first() - num.second();
+    } else if (num.operator == "*") {
+      return num.first() * num.second();
+    } else if (num.operator == "/") {
+      return num.first() / num.second();
     }
+  }
+}
+
+$(".number").click(function (event) {
+  if (error != true) { select(writeNumber); }
+});
+
+$(".zero").click(function (event) {
+  if (error != true) { select(writeZero); }
+});
+
+$(".point").click(function (event) {
+  if (error != true) { select(writePoint); }
+});
+
+$(".operation").click(function (event) {
+  if (error != true) {
+    if (seq.second.length == 0) {
+      num.operator = event.currentTarget.id;
+    } else if (lastClicked == "=") {
+      seq.second = [];
+      num.operator = event.currentTarget.id;
+    } else {
+      seq.first = [num.result()];
+      num.first();
+      check();
+      seq.second = [];
+      num.second();
+      num.operator = event.currentTarget.id;
+    }
+    activeArray = "first";
+    markLast();
   }
 });
 
-function activateAll(){
-  $(".number").click(function(event){
-    activate(write);
-  });
-
-  $(".zero").click(function(event){
-    activate(writeZero);
-  });
-
-  $(".point").click(function(event){
-    activate(writePoint);
-  });
-
-  $(".del").click(function(event){
-    activateDel();
-  });
-
-  $(".operation").click(function(event){
-    activateOperation();
-  });
-
-  $(".equal").click(function(event){
-    activateEqual();
-  });
-}
-
-function activate(e){
-  if(res.length == 0 || lastClicked == "="){
-    res = [];
-    e("first");
+$(".equal").click(function (event) {
+  if (error != true) {
+    if (operators.includes(lastClicked) == true) {
+      if ((lastClicked == "+" && num.first().toString().includes("-")) || lastClicked == "-") {
+        num.operator = "*";
+        seq.second = [-1];
+        seq.first = [num.result()];
+        seq.second = [];
+        num.second();
+        check();
+      }
+    } else {
+      if (seq.second.length == 0) {
+        display("first");
+      } else {
+        seq.first = [num.result()];
+        num.first();
+        check();
+      }
+    }
     activeArray = "first";
-  }else{
-    e("second");
+    markLast();
+  }
+});
+
+$(".del").click(function (event) {
+  if (error != true) {
+    if (lastClicked == "=" || operators.includes(lastClicked) == true) {
+      seq.second = [];
+      num.second();
+      num.operator = 0;
+    }
+    seq[activeArray].pop();
+    num[activeArray]();
+    display(activeArray);
+    if(seq[activeArray][seq[activeArray].length - 1] == "."){
+      $(".field").text(num[activeArray]() + ".");
+    }
+    markLast();
+  }
+});
+
+$(".reset").click(function (event) {
+  seq = { first: [], second: [] };
+  num.first();
+  num.second();
+  num.operator = 0;
+  num.result();
+  display("first");
+  error = false;
+  markLast();
+});
+
+function select(writeIt) {
+  if (num.operator == 0) {
+    writeIt("first");
+    activeArray = "first";
+  } else if (lastClicked == "=") {
+    num.operator = 0;
+    seq = { first: [], second: [] };
+    writeIt("first");
+  } else {
+    writeIt("second");
     activeArray = "second";
   }
   markLast();
 }
 
-function activateDel(){
-  if(seq[activeArray].length > 1){
-    if(activeArray != null){
-      seq[activeArray].pop();
-      num[activeArray] = seq[activeArray].join('');
-      display(activeArray);
+function writeNumber(n) {
+  if (seq[n].length < 10) {
+    if(lastClicked == "="){
+      seq[n] = [];
     }
-  }else{
-    seq[activeArray].pop();
-    num[activeArray] = 0;
-    display(activeArray);
-  }
-  markLast();
-}
-
-function activateOperation(){
-  if(seq.first.length != 0 || lastClicked == "="){
-    if(operators.some(el => res.includes(el)) == false){
-      res.push(num.first, event.currentTarget.id);
-    }else if(seq.second.length != 0){
-      res.push(num.second);
-      num.first = +eval(res.join('')).toFixed(8);
-      seq.second = [];
-      num.second = 0;
-      res = [num.first, event.currentTarget.id];
-      checkForError();
-      if(error == false){
-        display("first");
-      }
-    }else if(operators.includes(res[1]) == true){
-      res.pop();
-      res.push(event.currentTarget.id);
-    }
-  }else if(event.currentTarget.id == "-"){
-    seq.first.push("-");
-  }
-  markLast();
-}
-
-function activateEqual(){
-  if(operators.includes(lastClicked) == true){
-    if((lastClicked == "+" && num.first.toString().includes("-")) || lastClicked == "-"){
-      num.first = num.first * (-1);
-      seq.first = [];
-    }
-    display("first");
-    num.second = 0;
-    res = [num.first, "+"];
-  }else{
-    if(res.length == 0){
-      display("first");
-      seq.first = [];
-    }else{
-      res.push(num.second);
-      num.first = +eval(res.join('')).toFixed(8);
-      res.splice(0, 1, num.first);
-      res.pop();
-      seq = {first:[], second:[]};
-      checkForError();
-      if(error == false){
-        display("first");
-      }
-    }
-  }
-  markLast();
-}
-
-function write(n){
-  if(seq[n][0] == 0 && seq[n][1] != "."){
-    seq[n].pop();
-  }
-  if(seq[n].length < 10){
     seq[n].push(event.currentTarget.id);
-    num[n] = seq[n].join('');
+    num[n]();
+    if(seq[n].includes(".") && event.currentTarget.id == "0"){
+      $(".field").text(seq[n].join(''));
+    }else{
     display(n);
+    }
   }
 }
 
-function writeZero(n){
-  if(seq[n][0] != 0 || seq[n][1] == "."){
-    write(n);
+function writeZero(n) {
+  if (seq[n][0] != 0 || seq[n][1] == ".") {
+    writeNumber(n);
   }
 }
 
-function writePoint(n){
-  if(seq[n].includes(".") == false){
+function writePoint(n) {
+  if (seq[n].includes(".") == false) {
     seq[n].push(".");
-    if(seq[n][0] == "."){
+    if (seq[n][0] == ".") {
       seq[n].splice(0, 0, 0);
     }
   }
-  num[n] = Number(seq[n].join(''));
+  num[n]();
   display(n);
 }
 
-function display(n){
-  $(".field").text(num[n]);
+function display(n) {
+  $(".field").text(num[n]());
 }
 
-function markLast(){
+function markLast() {
   lastClicked = event.currentTarget.id;
 }
 
-function checkForError(){
+function check() {
   reduceDecimal();
-  if(num.first == Infinity || num.first.toString().length > 10){
-    showError();
-  }else if(num.first.toString().includes("e") == true){
-    num.first = 0;
-    res.splice(0, 1, 0);
+  if (num.first() == Infinity || num.first().toString().length > 10) {
+    if (num.first().toString().includes(".") == false) {
+      showError();
+    } else if (num.first().toString().includes("e") == true) {
+      seq = { first: [], second: [] };
+      num.first();
+      display("first");
+    }
+  }
+}
+
+function reduceDecimal() {
+  if (num.first().toString().includes(".") == true && num.first().toString().length > 10) {
+    $(".field").text(num.first().toString().slice(0, num.first().toString().length - (num.first().toString().length - 10)));
+  } else {
     display("first");
   }
 }
 
-function reduceDecimal(){
-  num.first = num.first.toString();
-  if(num.first.includes(".") == true && num.first.length > 10){
-    num.first = num.first.slice(0, num.first.length - (num.first.length - 10));
-    res.splice(0, 1, num.first);
-  }
-  num.first = Number(num.first);
-}
-
-function showError(){
-  $(".field").text("E");
+function showError() {
+  $(".field").text("ERR");
   error = true;
-  $(".number").off("click");
-  $(".zero").off("click");
-  $(".point").off("click");
-  $(".del").off("click");
-  $(".operation").off("click");
-  $(".equal").off("click"); 
 }
